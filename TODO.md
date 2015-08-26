8791f872e3e31d78e8932912cf7cb76611116034
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2015-08-26 22:46:31 +0200

    Some browsing improvements

--- TODO.md
+++ TODO.md
@@ -1,3 +1,4 @@
 1. get right pictures, text and audio using js
 2. display map and let user move and zoom on it
 3. when closing, stop audio
+4. add history navigation + document.title
--- js/logic.js
+++ js/logic.js
@@ -7,33 +7,33 @@ $(function() {
   var hash = '';
   hash = location.hash;
   
-  if (hash == '' )
-    return;  //do nothing
-  
-  var placeId = hash.substring(1,hash.length)
-  var dir = rootDir + '/' + placeId + '/';
-  var title = '<h2>Título del lugar</h2>'
-  document.getElementById("dialog_title").innerHTML = title
-  var text =  '<p align="left">Texto informativo para el lugar.<br/></p>'//dir+'texto.txt');
-  document.getElementById("dialog_text").innerHTML = text
-  $('#dialog_img_src').attr("src",dir+placeId+'.png');
-  $('#dialog_audio_src').attr("src",dir+placeId+'.mp3');
-  $("#dialog_audio_player").load();
-  
-  
-  open_dialog();
- /*hashwith = '#'+hash.substring(7, hashlenght);
- hash = location.hash.substring(7, hashlenght);
- if(hashwith != "#"+'index'){
- show_overlay(hash);
- document.title = 'Work '+hash;
- }
- else{
- close_overlay();
-  
- }*/
-  
- });
+  if (hash == '' && $(dialog).is(":visible"))
+    close_dialog();
+  else if (hash != ''){
+    var placeId = hash.substring(1,hash.length)
+    var dir = rootDir + '/' + placeId + '/';
+    var title = '<h2>Título del lugar</h2>'
+    document.getElementById("dialog_title").innerHTML = title
+    var text =  '<p align="left">Texto informativo para el lugar.<br/></p>'//dir+'texto.txt');
+    document.getElementById("dialog_text").innerHTML = text
+    $('#dialog_img_src').attr("src",dir+placeId+'.png');
+    $('#dialog_audio_src').attr("src",dir+placeId+'.mp3');
+    $("#dialog_audio_player").load();
+    
+    
+    open_dialog();
+   /*hashwith = '#'+hash.substring(7, hashlenght);
+   hash = location.hash.substring(7, hashlenght);
+   if(hashwith != "#"+'index'){
+   show_overlay(hash);
+   document.title = 'Work '+hash;
+   }
+   else{
+   close_overlay();
+    
+   }*/
+  }
+});
  
  $(window).trigger( 'hashchange' );
   
@@ -47,8 +47,9 @@ function close_dialog() {
   $(transbox).hide('slow')
   $(dialog).hide('slow')
   $(document).unbind('keyup.esc')
-  location.hash = '';
-  //document.location="./mapa-sonoro.html"
+  
+  // going back to mapa-sonoro.html:
+  history.pushState("", document.title, window.location.pathname + window.location.search);
 }
 
 function open_dialog() {
@@ -75,4 +76,3 @@ function open_dialog() {
   });
 }
 
-1
