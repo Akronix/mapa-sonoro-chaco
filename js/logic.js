bee7d381d59a6d5990a868c1a05d3f2de86f17d4
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2015-08-26 21:18:18 +0200

    Closer to getting right pictures, text and audio

---
+++ js/logic.js
@@ -0,0 +1,78 @@
+var rootDir = 'material'
+
+$(function() {
+  
+  //set up hash detection 
+  $(window).bind( 'hashchange', function(e) {
+  var hash = '';
+  hash = location.hash;
+  
+  if (hash == '' )
+    return;  //do nothing
+  
+  var placeId = hash.substring(1,hash.length)
+  var dir = rootDir + '/' + placeId + '/';
+  var title = '<h2>Título del lugar</h2>'
+  document.getElementById("dialog_title").innerHTML = title
+  var text =  '<p align="left">Texto informativo para el lugar.<br/></p>'//dir+'texto.txt');
+  document.getElementById("dialog_text").innerHTML = text
+  $('#dialog_img_src').attr("src",dir+placeId+'.png');
+  $('#dialog_audio_src').attr("src",dir+placeId+'.mp3');
+  $("#dialog_audio_player").load();
+  
+  
+  open_dialog();
+ /*hashwith = '#'+hash.substring(7, hashlenght);
+ hash = location.hash.substring(7, hashlenght);
+ if(hashwith != "#"+'index'){
+ show_overlay(hash);
+ document.title = 'Work '+hash;
+ }
+ else{
+ close_overlay();
+  
+ }*/
+  
+ });
+ 
+ $(window).trigger( 'hashchange' );
+  
+  /*$(".places_lnk").each(
+    function(){
+      $(this).click( open_dialog )});*/
+});
+
+
+function close_dialog() {
+  $(transbox).hide('slow')
+  $(dialog).hide('slow')
+  $(document).unbind('keyup.esc')
+  location.hash = '';
+  //document.location="./mapa-sonoro.html"
+}
+
+function open_dialog() {
+  // unfocus and desactivate any other component
+  document.getElementById("map_container").onclick=close_dialog
+  
+  // abrir diálogo
+  
+  // poner imagen de cargando
+  
+  // load data and media
+  //dialog.css('position:');*/
+  
+  // Change web title
+  //document.title += ;
+  
+  // Una vez cargadas las imágenes el texto y el audio, mostrar todo.
+  $(transbox).show('slow')
+  $(dialog).show('slow') 
+  
+  // Adding esc event
+  $(document).on('keyup.esc',function(e) {if (e.keyCode == 27) /* escape key */
+    close_dialog();
+  });
+}
+
+1
