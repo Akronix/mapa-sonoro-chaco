7e984ef6eb30ce26d0f71e3d7df226a92fc82e4b
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2015-09-09 02:43:09 +0200

    middle commit - working for 7 and some improvements/additions

--- js/logic.js
+++ js/logic.js
@@ -1,5 +1,12 @@
 $(function() {
   
+  /* Doesn't work I don't know why :'( */ 
+   /* At start up, center screen on stating point of the map */
+    //var wOffSet = $("#scrollable_container").width() / 2, hOffSet = $("#scrollable_container").height() / 2;
+    //document.getElementById("scrollable_container").scrollTop = 1400 - hOffSet;
+    //document.getElementById("scrollable_container").scrollLeft= 640 + wOffSet;
+  
+  
   $('#dialog_img').click(function() {
     var player = document.getElementById('dialog_audio_player')
     if (player.paused == false)
